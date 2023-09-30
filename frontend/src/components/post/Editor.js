7f815c1562e03bb2c9838e9011e6623a5167b5a9
park cho hye <chohye@172.20.nate.com>
2023-09-30 14:18:08 +0900

    refactor : 에디터 모듈 렌더링 최적화

--- frontend/src/components/post/Editor.js
+++ frontend/src/components/post/Editor.js
@@ -1,9 +1,44 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactQuill from "react-quill";
+import { useMemo } from "react";
 import "react-quill/dist/quill.snow.css";
 
 const Editor = ({ post, setPost }) => {
+  const modules = useMemo(() => {
+    return {
+      toolbar: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+        ["link", "image"],
+        [{ color: [] }, { background: [] }],
+        ["clean"],
+      ],
+      clipboard: {
+        matchVisual: false,
+      },
+    };
+  }, []);
+
+  const formats = [
+    "color",
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   const handleChange = (html) => {
     console.log(html);
     setPost((prev) => ({ ...prev, contents: html }));
@@ -21,34 +56,5 @@ const Editor = ({ post, setPost }) => {
     );
   }
 };
-const modules = {
-  toolbar: [
-    [{ header: "1" }, { header: "2" }, { font: [] }],
-    [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
-    ["link", "image"],
-    ["clean"],
-  ],
-  clipboard: {
-    matchVisual: false,
-  },
-};
-
-const formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-];
 
 export default Editor;
